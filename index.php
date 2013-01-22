ef1bfaa00a7d773105419eb827fbe0cd9fd72b76
Jake Spurlock <whyisjake@gmail.com>
2013-01-22 15:11:09 -0800

    Trying to make it look a lot prettier...

--- index.php
+++ index.php
@@ -1,30 +1,39 @@
 <?php
 
-// Add a query parameter for playlists.
+/***************/
+/** YouTube to WordPress XML
+/** https://github.com/whyisjake/YouTube-to-WordPress-XML
+/** Description: Generate WordPress XML exports from YouTube playlists.
+/** Author: Jake Spurlock
+/** Version: 0.5
+/***************/
+
+/***************/
+/** WordPress **/
+/***************/
+
+//Path to the WordPress files. Eventually, I'll strip the used functions out of WordPress, and add them to this file.
+require('../../wp-load.php');
+
+
+/***************/
+/** Variables **/
+/***************/
+
+$GLOBALS['t'] = '$t';
 $limit = (!empty($_REQUEST['offset']) ? $_REQUEST['offset'] : null);
-
-//Username
 $username = (!empty($_REQUEST['username']) ? $_REQUEST['username'] : null);
-
-//Start Index
 $start = (!empty($_REQUEST['start']) ? $_REQUEST['start'] : null);
+
+/***************/
+/** Functions **/
+/***************/
 
 // Change the names of categories be dashed, rather then have spaces.
 function make_dashed($str) {
 	$dashed = str_replace(' ', '-', $str );
 	return $dashed;
 }
-
-
-// GLobals //
-
-$GLOBALS['t'] = '$t';
-$GLOBALS['playlist'] = $videosobj->feed->title->$t;
-
-
-
-//Path to the WordPress files.
-require('../../wp-load.php');
 
 function make_get_the_playlist_feed( $limit, $username, $start ) {
 
@@ -40,10 +49,46 @@ function make_get_the_playlist_feed( $limit, $username, $start ) {
 	$contents = file_get_contents($url);
 	$videosobj = json_decode($contents);
 	$videos = $videosobj->feed->entry;
-	return $videos;
+	// return $videos;
+	// return $GLOBALS['playlist'] = $videosobj->feed->title->$t;
+	$t = '$t';
+	$output = array(
+		'playlist' => $videosobj->feed->title->$t,
+		'videos' => $videos
+		);
+	return $output;
 }
 
-$videos = make_get_the_playlist_feed( $limit, $username, $start );
+function make_video_item_builder( $videos ) {
+	$playlist = $videos['playlist'];
+
+	foreach ( $videos['videos'] as $video ) {
+		$t = '$t';
+		$link = $video->link;
+		echo "\t" . '<item>' . "\n";
+		echo "\t\t" . '<title>' . ent2ncr( esc_html( $video->title->$t ) ) . '</title>' . "\n";
+		echo "\t\t" . '<pubDate>' . $video->published->$t . '</pubDate>' . "\n";
+		echo "\t\t" . '<dc:creator>makemagazine</dc:creator>' . "\n";
+		echo "\t\t" . '<description></description>' . "\n";
+		echo "\t\t" . '<content:encoded><![CDATA[[youtube="' . $link[0]->href . '"]' . "\n" . $video->content->$t . ']]></content:encoded>' . "\n";
+		echo "\t\t" . '<excerpt:encoded><![CDATA[' . $video->content->$t . ']]></excerpt:encoded>' . "\n";
+		echo "\t\t" . '<wp:post_id>' . mt_rand(0, 2000) . '</wp:post_id>' . "\n";
+		echo "\t\t" . '<wp:post_date>' . $video->published->$t . '</wp:post_date>' . "\n";
+		echo "\t\t" . '<wp:comment_status>open</wp:comment_status>' . "\n";
+		echo "\t\t" . '<wp:ping_status>open</wp:ping_status>' . "\n";
+		echo "\t\t" . '<wp:post_name>' . ent2ncr( esc_html( $video->title->$t ) ) . '</wp:post_name>' . "\n";
+		echo "\t\t" . '<wp:status>publish</wp:status>' . "\n";
+		echo "\t\t" . '<wp:post_type>video</wp:post_type>' . "\n";
+		echo "\t\t" . '<wp:post_parent>0</wp:post_parent>' . "\n";
+		echo "\t\t" . '<wp:menu_order>0</wp:menu_order>' . "\n";
+		echo "\t\t" . '<wp:post_type>post</wp:post_type>' . "\n";
+		echo "\t\t" . '<wp:post_password></wp:post_password>' . "\n";
+		echo "\t\t" . '<wp:is_sticky>0</wp:is_sticky>' . "\n";
+		echo "\t\t" . '<category domain="category" nicename="science-technology"><![CDATA[Science &amp; Technology]]></category>' . "\n";
+		echo "\t\t" . '<category domain="playlist" nicename="' . make_dashed( $playlist ) . '"><![CDATA[' . $playlist . ']]></category>' . "\n";
+		echo "\t" . '</item>' . "\n";
+  	}
+}
 
 //Spit out the header with the correct content type.
 header("Content-type: text/xml; charset=utf-8");
@@ -72,34 +117,7 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 	<wp:wxr_version>1.2</wp:wxr_version>
 	<generator>https://github.com/whyisjake/YouTube-to-WordPress-XML</generator>
 
-<?php 
-	foreach ( $videos as $video ) {
-		$t = '$t';
-		$link = $video->link;
-		echo "\t" . '<item>' . "\n";
-		echo "\t\t" . '<title>' . ent2ncr( esc_html( $video->title->$t ) ) . '</title>' . "\n";
-		echo "\t\t" . '<pubDate>' . $video->published->$t . '</pubDate>' . "\n";
-		echo "\t\t" . '<dc:creator>makemagazine</dc:creator>' . "\n";
-		echo "\t\t" . '<description></description>' . "\n";
-		echo "\t\t" . '<content:encoded><![CDATA[[youtube="' . $link[0]->href . '"]' . "\n" . $video->content->$t . ']]></content:encoded>' . "\n";
-		echo "\t\t" . '<excerpt:encoded><![CDATA[' . $video->content->$t . ']]></excerpt:encoded>' . "\n";
-		echo "\t\t" . '<wp:post_id>' . mt_rand(0, 2000) . '</wp:post_id>' . "\n";
-		echo "\t\t" . '<wp:post_date>' . $video->published->$t . '</wp:post_date>' . "\n";
-		echo "\t\t" . '<wp:comment_status>open</wp:comment_status>' . "\n";
-		echo "\t\t" . '<wp:ping_status>open</wp:ping_status>' . "\n";
-		echo "\t\t" . '<wp:post_name>' . ent2ncr( esc_html( $video->title->$t ) ) . '</wp:post_name>' . "\n";
-		echo "\t\t" . '<wp:status>publish</wp:status>' . "\n";
-		echo "\t\t" . '<wp:post_type>video</wp:post_type>' . "\n";
-		echo "\t\t" . '<wp:post_parent>0</wp:post_parent>' . "\n";
-		echo "\t\t" . '<wp:menu_order>0</wp:menu_order>' . "\n";
-		echo "\t\t" . '<wp:post_type>post</wp:post_type>' . "\n";
-		echo "\t\t" . '<wp:post_password></wp:post_password>' . "\n";
-		echo "\t\t" . '<wp:is_sticky>0</wp:is_sticky>' . "\n";
-		echo "\t\t" . '<category domain="category" nicename="science-technology"><![CDATA[Science &amp; Technology]]></category>' . "\n";
-		echo "\t\t" . '<category domain="playlist" nicename="' . make_dashed( $playlist ) . '"><![CDATA[' . $playlist . ']]></category>' . "\n";
-		echo "\t" . '</item>' . "\n";
-  	}
- ?>
+	<?php make_video_item_builder( make_get_the_playlist_feed( $limit, $username, $start ) ); ?>
 		
 </channel>
 </rss>
